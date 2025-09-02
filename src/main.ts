963d098cb2140c9bcbcd533836fc6c0b23fa9e3f
RumBaughNam <expoool@me.com>
2025-09-03 02:03:34 +0900

    CORS 처리

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,                  // 필요시 도메인 화이트리스트로 교체
+    origin: 'https://rumbaugh.co.kr',                  // 필요시 도메인 화이트리스트로 교체
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
